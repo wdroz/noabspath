9c4a344de8ab8dee8cc121f3e9ff2d7cf4c18b50
William Droz <william.droz.ch@gmail.com>
2020-07-19 11:47:31 +0200

    refactor by using trait and structs

--- src/checkabspath/mod.rs
+++ src/checkabspath/mod.rs
@@ -6,6 +6,11 @@ use std::path::{Path, PathBuf};
 use std::fs;
 use std::fmt;
 
+/// Simple trait to detect if the path exist from a string. Return empty string if no match
+pub trait PathDetection {
+    fn path_exist(&self, line: &String) -> String;
+}
+
 /// When a path is found
 pub struct PathFinded {
     /// The path to where the file is
@@ -21,6 +26,43 @@ impl fmt::Display for PathFinded{
         write!(f, "[ {} ] File {:<20} line {:>04} --> {:<20}", "x".red().bold(), self.filepath, self.line_number, self.path)
     }
 }
+/// Specific regex for paths
+pub struct RegExForPath {
+    regex: Regex,
+}
+
+/// Use internal regex to check for a path
+impl PathDetection for RegExForPath {
+    fn path_exist(&self, line: &String) -> String {
+        let mut res: String = String::from(""); 
+        for caps in self.regex.captures_iter(line) {
+            for cap in caps.iter() {
+                res = cap.unwrap().as_str().to_string();
+                return res;
+            }
+        }
+        res
+    }
+    
+}
+/// Set of specific regex for paths
+pub struct RegExSetForPath {
+    regex_set: Vec<RegExForPath>,
+}
+
+/// Use all specific regex in a loop to search for paths
+impl PathDetection for RegExSetForPath {
+    fn path_exist(&self, line: &String) -> String {
+        let mut res: String = String::from(""); 
+        for regex in &self.regex_set[..] {
+            res = regex.path_exist(line);
+            if !res.is_empty(){
+                return res;
+            }
+        }
+        res
+    }
+}
 
 /// Check if a codebase has any absolute paths
 ///
@@ -29,8 +71,12 @@ impl fmt::Display for PathFinded{
 /// * `path` - The location of the codebase
 ///
 pub fn check_codebase(path: String) -> Result<(), Vec<PathFinded>> {
-    let set = vec![Regex::new("[\"']/\\w+/?[^'\"]+[\"']").unwrap(),
-                   Regex::new("[\"']\\\\w+\\?[^'\"]+[\"']").unwrap(),];
+    let set = RegExSetForPath {
+        regex_set: vec![
+            RegExForPath {regex: Regex::new("[\"']/\\w+/?[^'\"]+[\"']").unwrap()},
+            RegExForPath {regex: Regex::new("[\"']\\\\w+\\?[^'\"]+[\"']").unwrap()},
+        ],
+    };
     let mut glob_expression = path;
     if glob_expression.ends_with("/") {
         glob_expression.push_str("**/*");
@@ -54,9 +100,9 @@ pub fn check_codebase(path: String) -> Result<(), Vec<PathFinded>> {
 /// # Arguments
 ///
 /// * `path` - Location of the file
-/// * `set` - Vector of Regex that will be use to search for absolute paths
+/// * `set` - Implementation of PathDetection that will be use to search for absolute paths
 ///
-fn check_entry(path: &Path, set: &Vec<Regex>) -> Option<Vec<PathFinded>>
+fn check_entry(path: &Path, set: &impl PathDetection) -> Option<Vec<PathFinded>>
 {
     let metadata = path.metadata();
     match metadata {
@@ -77,22 +123,19 @@ fn check_entry(path: &Path, set: &Vec<Regex>) -> Option<Vec<PathFinded>>
 /// # Arguments
 ///
 /// * `lines` - Content of a file
-/// * `set` - Vector of Regex that will be use to search for absolute paths
+/// * `set` - Implementation of PathDetection that will be use to search for absolute paths
 /// * `file` - Path to the initial file (useful to build PathFinded instances)
 ///
-fn fill_from_content(lines: &String, set: &Vec<Regex>, file: &Path) -> Vec<PathFinded> {
+fn fill_from_content(lines: &String, set: &impl PathDetection, file: &Path) -> Vec<PathFinded> {
     let mut res = std::vec::Vec::new();
     for (nb, line) in lines.lines().enumerate(){
-        for regex in set {
-            for caps in regex.captures_iter(&line) {
-                for cap in caps.iter() {
-                    res.push(PathFinded {
-                        filepath: file.to_str().unwrap().to_string(), 
-                        line_number: 1+nb as u64, 
-                        path: cap.unwrap().as_str().to_string(),
-                        });
-                }
-            }
+        let path = set.path_exist(&line.into());
+        if !path.is_empty(){
+            res.push(PathFinded {
+                filepath: file.to_str().unwrap().to_string(), 
+                line_number: 1+nb as u64, 
+                path: path,
+                });
         }
     }
     res
@@ -103,9 +146,9 @@ fn fill_from_content(lines: &String, set: &Vec<Regex>, file: &Path) -> Vec<PathF
 /// # Arguments
 ///
 /// * `path` - Location of the file
-/// * `set` - Vector of Regex that will be use to search for absolute paths
+/// * `set` - Implementation of PathDetection that will be use to search for absolute paths
 ///
-fn check_one_file(file: &Path, set: &Vec<Regex>) -> Option<Vec<PathFinded>> {
+fn check_one_file(file: &Path, set: &impl PathDetection) -> Option<Vec<PathFinded>> {
     let contents = fs::read_to_string(file);
     match contents {
         Ok(lines) => {
@@ -129,16 +172,16 @@ mod checkabspath {
 
     #[test]
     fn test_check_one_file_regex_find() {
-        let set = vec![Regex::new(r"William Droz").unwrap()];
+        let regex = RegExForPath {regex: Regex::new(r"William Droz").unwrap()};
         let my_file = Path::new("src/checkabspath/mod.rs");
-        let res = check_one_file(&my_file, &set);
+        let res = check_one_file(&my_file, &regex);
         assert!(res.is_some())
     }
     #[test]
     fn test_check_one_file_regex_not_find() {
-        let set = vec![Regex::new(r"William Droz").unwrap()];
+        let regex = RegExForPath {regex: Regex::new(r"William Droz").unwrap()};
         let my_file = Path::new(".gitignore");
-        let res = check_one_file(&my_file, &set);
+        let res = check_one_file(&my_file, &regex);
         assert!(res.is_none())
     }
 }
