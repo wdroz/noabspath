ab99e7dc3a236b714ce76163b8eae763976950f5
William Droz <william.droz@idiap.ch>
2022-04-21 19:39:15 +0200

    Use latest clap syntax

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,27 @@
-#[macro_use]
-extern crate clap;
+use clap::Parser;
 use colored::*;
 
 mod checkabspath;
 
 use checkabspath::check_codebase;
 
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Path of codebase to check
+    #[clap(short, long)]
+    path: String,
+
+    /// File that contains patterns to ignore
+    #[clap(short, long, default_value = ".gitignore")]
+    ignore_file: String,
+}
+
+
 fn main() {
-    let matches = clap_app!(noabspath =>
-        (version: "0.1.6")
-        (author: "William Droz <william.droz.ch@gmail.com>")
-        (about: "check that there aren't obvious absolute paths in codebases")
-        (@arg PATH: +required "path of codebase to check")
-        (@arg IGNORE_FILE: -i --ignore_file +takes_value "File that contains patterns to ignore (default .gitignore)")
-    ).get_matches();
-    let folder = matches.value_of("PATH").unwrap();
-    let ignore_file = matches
-        .value_of("IGNORE_FILE")
-        .unwrap_or(".gitignore")
-        .to_string();
+    let args = Args::parse();
+    let folder = args.path;
+    let ignore_file = args.ignore_file;
     println!("check absolute path in : {}", folder);
     let res = check_codebase(folder.to_string(), ignore_file);
     match res {
